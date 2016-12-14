586252945caf1b5928ce7bb36e69d6d686329973
Raymond Lee <hoyenlee@yahoo.com>
2016-12-14 16:28:43 -0500

    updates with www2-static

--- app/views/tour-of-heroes.blade.php
+++ app/views/tour-of-heroes.blade.php
@@ -4,7 +4,6 @@
 <base href="/www2/public/index.php/tour-of-heroes" />
 <meta charset="utf-8">
 <title>Angular 2 - Todo Example</title>
-<!-- <link rel="stylesheet" type="text/css" href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"> -->
 <link rel="stylesheet" type="text/css" href="/www2-static/build/Release/css/global-styles.css">
 </head>
 
@@ -13,12 +12,14 @@
     <script src="//unpkg.com/zone.js@0.6.25?main=browser"></script>
     <script src="//unpkg.com/reflect-metadata@0.1.8"></script>
     <script src="//unpkg.com/systemjs@0.19.39/dist/system.src.js"></script>
+    
 <script>
 (function(){
 	window.MySite = {
 		appPath: '/tour-of-heroes',
 		templateSrc: '/www2-static/build/Release/templates/ng2/tour-of-heroes/',
-		stylesSrc: 'www2-static/build/Release/css/ng2-apps/tour-of-heroes/'
+		stylesSrc: 'www2-static/build/Release/css/ng2-apps/tour-of-heroes/',
+		mainSrc: './tour-of-heroes/main.js',
 	}
 	var paths = window.location.pathname.split(window.MySite.appPath);
 	var startPaths = paths[0];
@@ -29,54 +30,20 @@
 	});
 	window.MySite.stylesSrc=prefix+window.MySite.stylesSrc;
 })();
-
-(function (global) {
-  System.config({
-    paths: {
-      // paths serve as alias
-      //'npm:': '/node_modules/'
-	  'npm:': '//unpkg.com/'
-    },
-    // map tells the System loader where to look for things
-    map: {
-      // our app is within the app folder
-      app: '/build/Release/js/ng2-apps',
-
-      // angular bundles
-      '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
-      '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
-      '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
-      '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
-      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-      '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
-      '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
-      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-
-      // other libraries
-      'rxjs':  'npm:rxjs',
-      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
-    },
-    // packages tells the System loader how to load when no filename and/or no extension
-    packages: {
-		/*
-      app: {
-        main: './tour-of-heroes/main.js',
-        defaultExtension: 'js'
-      },
-	  */
-      rxjs: {
-        defaultExtension: 'js'
-      }
-    }
-  });
-})(this);
-
 </script>
-<!-- 3. Bootstrap --> 
-<script src="/www2-static//build/Release/js/ng2-apps/tour-of-heroes/tour-of-heroes.combo.min.js"></script>
-<script>
-	System.import('main').catch(function(err){ console.error(err); });
-</script>
+@if (App::environment()==='local')
+	<script src="/www2-static//build/Release/js/ng2-apps/systemjs-config-dev.js"></script>
+	<script>
+        System.import('app').catch(function(err){ console.error(err); });
+    </script>
+@else
+	<script src="/www2-static//build/Release/js/ng2-apps/systemjs-config-production.min.js"></script>
+    <script src="/www2-static//build/Release/js/ng2-apps/tour-of-heroes/tour-of-heroes.combo.min.js"></script>
+	<script>
+        System.import('main').catch(function(err){ console.error(err); });
+    </script>
+@endif
+
 <div class="container theme-showcase" role="main">
   <my-app>Loading...</my-app>
 </div>
