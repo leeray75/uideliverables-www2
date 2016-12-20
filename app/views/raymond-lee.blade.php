c4f36cb7b1046e761815cadc7899e71e32699238
Raymond Lee <hoyenlee@yahoo.com>
2016-12-20 17:32:23 -0500

    raymond-lee: remove seo values

--- app/views/raymond-lee.blade.php
+++ app/views/raymond-lee.blade.php
@@ -2,12 +2,12 @@
 <!doctype html>
 <html lang="en">
 <head>
-<title>UI Deliverables</title>
+<title></title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <meta name="author" content="Raymond Lee" />
-<meta name="description" content="Welcome to UI Deliverables! I am a front-end developer with experience in developing in HTML/HTML5, JavaScript, CSS/CSS3.">
-<meta name="keywords" content="HTML,HTML5,CSS,CSS3,angular,angularjs,JavaScript,jQuery,resume,portfolio,demos,contact">
+<meta name="description" content="">
+<meta name="keywords" content="">
 
 <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico">
 <link href='//fonts.googleapis.com/css?family=Lobster|Open+Sans:400,300,300italic,600,400italic,600italic,700,700italic,800,800italic|Roboto+Condensed:400,300,300italic,400italic,700,700italic|Source+Sans+Pro:400,200,200italic,300,300italic,400italic,600,600italic,700,700italic,900,900italic|PT+Sans:400,400italic,700,700italic|Droid+Serif:400,400italic,700,700italic|Ubuntu:400,300,300italic,400italic,500,500italic,700,700italic|Merriweather:400,300,300italic,400italic,700,700italic,900,900italic|Play:400,700' rel='stylesheet' type='text/css'>
@@ -26,18 +26,6 @@
 	<link rel="stylesheet" type="text/css" href="/www2-static/build/Release/css/ng2-apps/ui-deliverables.css?v=<?php echo $cacheVersion ?>">
 @endif
 
-</head>
-
-<body role="document">
-	<section id="page" data-role="page">
-		  <ui-deliverables>Loading...</ui-deliverables>
-	</section>
-
-    <script src="//unpkg.com/core-js/client/shim.min.js"></script>
-    <script src="//unpkg.com/zone.js@0.6.25?main=browser"></script>
-    <script src="//unpkg.com/reflect-metadata@0.1.8"></script>
-    <script src="//unpkg.com/systemjs@0.19.39/dist/system.src.js"></script>
-    
 <script>
 (function(){
 	window.MySite = {
@@ -58,6 +46,19 @@
 	window.MySite.stylesSrc=prefix+window.MySite.stylesSrc;
 })();
 </script>
+
+</head>
+
+<body role="document">
+	<section id="page" data-role="page">
+		  <ui-deliverables>Loading...</ui-deliverables>
+	</section>
+
+    <script src="//unpkg.com/core-js/client/shim.min.js"></script>
+    <script src="//unpkg.com/zone.js@0.6.25?main=browser"></script>
+    <script src="//unpkg.com/reflect-metadata@0.1.8"></script>
+    <script src="//unpkg.com/systemjs@0.19.39/dist/system.src.js"></script>
+    
 @if (App::environment()==='local')
 	<script src="/www2-static/build/dev/js/ng2-apps/systemjs-config.js?v=<?php echo $cacheVersion ?>"></script>
 	<script>
